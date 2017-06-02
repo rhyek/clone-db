467fa170cd4f27599643b424dfa3525417499683
Carlos Gonzales <carlos.rgn@gmail.com>
2017-06-02 17:54:45 -0600

    no password for psql

--- engines/postgresql.js
+++ engines/postgresql.js
@@ -10,22 +10,22 @@ module.exports = async function (config) {
   const commands = [
     {
       message: `Generate and download ${ emoji.get(':poop:') } file.`,
-      command: `${ config.source.ssh ? `ssh -p ${ config.source.port || 22 } ${ config.source.host } ` : '' }PGPASSWORD="${ config.source.db.password }" pg_dump -O -Fc -h localhost -U ${ config.source.db.username } ${ config.source.db.database} > /tmp/clonedbdump`
+      command: `${ config.source.ssh ? `ssh -p ${ config.source.port || 22 } ${ config.source.host } ` : '' }${ config.source.db.password ? `PGPASSWORD="${ config.source.db.password }"` : '' } pg_dump -O -Fc -h localhost -U ${ config.source.db.username } ${ config.source.db.database} > /tmp/clonedbdump`
     },
     {
       message: 'Drop database at target location.',
-      command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }PGPASSWORD="${ config.target.db.password }" psql -h localhost -U ${ config.target.db.username } -c "drop database if exists ${ config.target.db.database };"`,
+      command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.target.db.password ? `PGPASSWORD="${ config.target.db.password }"` : '' } psql -h localhost -U ${ config.target.db.username } -c "drop database if exists ${ config.target.db.database };"`,
       skipWarnings: [
         'does not exist, skipping'
       ]
     },
     {
       message: 'Create database.',
-      command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }PGPASSWORD="${ config.target.db.password }" psql -h localhost -U ${ config.target.db.username } -c "create database ${ config.target.db.database };"`
+      command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.target.db.password ? `PGPASSWORD="${ config.target.db.password }"` : '' } psql -h localhost -U ${ config.target.db.username } -c "create database ${ config.target.db.database };"`
     },
     {
       message: 'Restore.',
-      command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }PGPASSWORD="${ config.target.db.password }" pg_restore -O -e -Fc -h localhost -U ${ config.target.db.username } -d ${ config.target.db.database } /tmp/clonedbdump`
+      command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.target.db.password ? `PGPASSWORD="${ config.target.db.password }"` : '' } pg_restore -O -e -Fc -h localhost -U ${ config.target.db.username } -d ${ config.target.db.database } /tmp/clonedbdump`
     }
   ]
   for (let { message, command, skipWarnings } of commands) {

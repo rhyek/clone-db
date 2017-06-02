d6c9472229cc28d86310efc2db93137652f0c02b
Carlos Gonzales <carlos.rgn@gmail.com>
2017-06-02 00:32:17 -0600

    some refactoring.

--- engines/mysql.js
+++ engines/mysql.js
@@ -5,50 +5,46 @@ const prettyMs = require('pretty-ms')
 const { timerStart, timerEnd, error } = require('../utils')
 const printError = error
 
-function handleError (error, stderr, spinner = null) {
-  function handle (thing) {
-    if (spinner) {
-      spinner.fail()
-    }
-    printError(thing)
-  }
-
-  if (error) {
-    handle (error)
-  }
-
-  stderr = stderr
-    .split('\n')
-    .filter(line => !line.includes('Using a password'))
-    .join()
-
-  if (stderr) {
-    handle (stderr)
-  }
-
-  if (spinner) {
-    spinner.succeed(`${ spinner.text } ${ prettyMs(timerEnd()) }`)
-  }
-}
-
-module.exports = function (config) {
+module.exports = async function (config) {
   timerStart('global')
-  timerStart()
-  let spinner = ora(`Generate and download ${ emoji.get(':poop:') } file.`).start()
-  exec(`${ config.source.ssh ? `ssh ${ config.source.host } ` : '' }mysqldump -u ${ config.source.db.username } --password=${ config.source.db.password} ${ config.source.db.database} | sed -e 's/DEFINER[ ]*=[ ]*[^*]*\\*/\\*/' > /tmp/clonedbdump`, (error, stdout, stderr) => {
-    handleError(error, stderr, spinner)
-
+  const commands = [
+    {
+      message: `Generate and download ${ emoji.get(':poop:') } file.`,
+      command: `${ config.source.ssh ? `ssh ${ config.source.host } ` : '' }mysqldump -u ${ config.source.db.username } --password=${ config.source.db.password} ${ config.source.db.database} | sed -e 's/DEFINER[ ]*=[ ]*[^*]*\\*/\\*/' > /tmp/clonedbdump`
+    },
+    {
+      message: 'Drop and create database at target location.',
+      command: `${ config.target.ssh ? `ssh ${ config.target.host } ` : '' }mysql -u ${ config.target.db.username } --password=${ config.target.db.password } -e 'drop database if exists ${ config.target.db.database }; create database ${ config.target.db.database };'`
+    },
+    {
+      message: 'Restore',
+      command: `${ config.target.ssh ? `ssh ${ config.target.host } ` : '' }mysql -u ${ config.target.db.username } --password=${ config.target.db.password } ${ config.target.db.database } < /tmp/clonedbdump`
+    }
+  ]
+  for (let { message, command } of commands) {
     timerStart()
-    spinner = ora(`Drop and create database at target location.`).start()
-    exec(`${ config.target.ssh ? `ssh ${ config.target.host } ` : '' }mysql -u ${ config.target.db.username } --password=${ config.target.db.password } -e 'drop database if exists ${ config.target.db.database }; create database ${ config.target.db.database };'`, (error, stdout, stderr) => {
-      handleError(error, stderr, spinner)
-
-      timerStart()
-      spinner = ora(`Restore.`).start()
-      exec(`${ config.target.ssh ? `ssh ${ config.target.host } ` : '' }mysql -u ${ config.target.db.username } --password=${ config.target.db.password } ${ config.target.db.database } < /tmp/clonedbdump`, (error, stdout, stderr) => {
-        handleError(error, stderr, spinner)
-        console.log(`Done! Total duration: ${ prettyMs(timerEnd('global')) }`)
+    let spinner = ora(message).start()
+    try {
+      await new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+          stderr = stderr
+            .split('\n')
+            .filter(line => !line.includes('Using a password'))
+            .join()
+          for (thing of [error, stderr]) {
+            if (thing) {
+              reject(thing)
+            }
+          }
+          resolve(stdout)
+        })
       })
-    })
-  })
+      spinner.succeed(`${ spinner.text } ${ prettyMs(timerEnd()) }`)
+    }
+    catch (error) {
+      spinner.fail()
+      printError(error)
+    }
+  }
+  console.log(`Done! Total duration: ${ prettyMs(timerEnd('global')) }`)
 }

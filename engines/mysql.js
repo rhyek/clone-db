53de1455ce7463b55b7e25be8c584dbb1fc3140b
Carlos Gonzales <carlos.rgn@gmail.com>
2017-06-08 20:44:56 -0600

    able to specify host in db

--- engines/mysql.js
+++ engines/mysql.js
@@ -4,8 +4,8 @@ module.exports = {
   commands (config) {
     return [
       {
-        message: `Generate and download ${ emoji.get(':poop:') } file.`,
-        command: `${ config.source.ssh ? `ssh -p ${ config.source.port || 22 } ${ config.source.host } ` : '' }${ config.source.db.password ? `MYSQL_PWD="${ config.source.db.password }"` : '' } mysqldump -u ${ config.source.db.username } -R ${ config.source.db.database} | sed -e 's/DEFINER[ ]*=[ ]*[^*]*\\*/\\*/' | sed -e 's/DEFINER[ ]*=[ ]*[^*]*PROCEDURE/PROCEDURE/' | sed -e 's/DEFINER[ ]*=[ ]*[^*]*FUNCTION/FUNCTION/' | gzip -c9 > /tmp/clonedbdump.gz`
+        message: `Generate and download ${ emoji.get(':poop:') }  file.`,
+        command: `${ config.source.ssh ? `ssh -p ${ config.source.port || 22 } ${ config.source.host } ` : '' }${ config.source.db.password ? `MYSQL_PWD="${ config.source.db.password }"` : '' } mysqldump -h ${ config.source.db.host ? config.source.db.host : 'localhost' } -u ${ config.source.db.username } -R ${ config.source.db.database} | sed -e 's/DEFINER[ ]*=[ ]*[^*]*\\*/\\*/' | sed -e 's/DEFINER[ ]*=[ ]*[^*]*PROCEDURE/PROCEDURE/' | sed -e 's/DEFINER[ ]*=[ ]*[^*]*FUNCTION/FUNCTION/' | gzip -c9 > /tmp/clonedbdump.gz`
       },
       {
         message: 'Decompress.',
@@ -13,19 +13,19 @@ module.exports = {
       },
       {
         message: 'Drop and create database at target location.',
-        command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.source.db.password ? `MYSQL_PWD="${ config.target.db.password }"` : '' } mysql -u ${ config.target.db.username } -e 'drop database if exists ${ config.target.db.database }; create database ${ config.target.db.database };'`
+        command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.source.db.password ? `MYSQL_PWD="${ config.target.db.password }"` : '' } mysql -h ${ config.target.db.host ? config.target.db.host : 'localhost' } -u ${ config.target.db.username } -e 'drop database if exists ${ config.target.db.database }; create database ${ config.target.db.database };'`
       },
       {
         message: 'Restore.',
-        command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.source.db.password ? `MYSQL_PWD="${ config.target.db.password }"` : '' } mysql -u ${ config.target.db.username } ${ config.target.db.database } < /tmp/clonedbdump`
+        command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.source.db.password ? `MYSQL_PWD="${ config.target.db.password }"` : '' } mysql -h ${ config.target.db.host ? config.target.db.host : 'localhost' } -u ${ config.target.db.username } ${ config.target.db.database } < /tmp/clonedbdump`
       },
       {
-        message: `Delete ${ emoji.get(':poop:') } file.`,
+        message: `Delete ${ emoji.get(':poop:') }  file.`,
         command: 'rm /tmp/clonedbdump*'
       }
     ]
   },
   runSQL (config, sql) {
-    return `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.source.db.password ? `MYSQL_PWD="${ config.target.db.password }"` : '' } mysql -u ${ config.target.db.username } -e "${ sql }" ${ config.target.db.database }`
+    return `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.source.db.password ? `MYSQL_PWD="${ config.target.db.password }"` : '' } mysql -h ${ config.target.db.host ? config.target.db.host : 'localhost' } -u ${ config.target.db.username } -e "${ sql }" ${ config.target.db.database }`
   }
 }
--- engines/postgresql.js
+++ engines/postgresql.js
@@ -4,31 +4,31 @@ module.exports = {
   commands (config) {
     return [
       {
-        message: `Generate and download ${ emoji.get(':poop:') } file.`,
-        command: `${ config.source.ssh ? `ssh -p ${ config.source.port || 22 } ${ config.source.host } ` : '' }${ config.source.db.password ? `PGPASSWORD="${ config.source.db.password }"` : '' } pg_dump -O -Fc -h localhost -U ${ config.source.db.username } ${ config.source.db.database} > /tmp/clonedbdump`
+        message: `Generate and download ${ emoji.get(':poop:') }  file.`,
+        command: `${ config.source.ssh ? `ssh -p ${ config.source.port || 22 } ${ config.source.host } ` : '' }${ config.source.db.password ? `PGPASSWORD="${ config.source.db.password }"` : '' } pg_dump -O -Fc -h ${ config.source.db.host ? config.source.db.host : 'localhost' } -U ${ config.source.db.username } ${ config.source.db.database} > /tmp/clonedbdump`
       },
       {
         message: 'Drop database at target location.',
-        command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.target.db.password ? `PGPASSWORD="${ config.target.db.password }"` : '' } psql -h localhost -U ${ config.target.db.username } -c "drop database if exists ${ config.target.db.database };"`,
+        command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.target.db.password ? `PGPASSWORD="${ config.target.db.password }"` : '' } psql -h ${ config.target.db.host ? config.target.db.host : 'localhost' } -U ${ config.target.db.username } -c "drop database if exists ${ config.target.db.database };"`,
         skipWarnings: [
           'does not exist, skipping'
         ]
       },
       {
         message: 'Create database.',
-        command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.target.db.password ? `PGPASSWORD="${ config.target.db.password }"` : '' } psql -h localhost -U ${ config.target.db.username } -c "create database ${ config.target.db.database };"`
+        command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.target.db.password ? `PGPASSWORD="${ config.target.db.password }"` : '' } psql -h ${ config.target.db.host ? config.target.db.host : 'localhost' } -U ${ config.target.db.username } -c "create database ${ config.target.db.database };"`
       },
       {
         message: 'Restore.',
-        command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.target.db.password ? `PGPASSWORD="${ config.target.db.password }"` : '' } pg_restore -O -e -Fc -h localhost -U ${ config.target.db.username } -d ${ config.target.db.database } /tmp/clonedbdump`
+        command: `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.target.db.password ? `PGPASSWORD="${ config.target.db.password }"` : '' } pg_restore -O -e -Fc -h ${ config.target.db.host ? config.target.db.host : 'localhost' } -U ${ config.target.db.username } -d ${ config.target.db.database } /tmp/clonedbdump`
       },
       {
-        message: `Delete ${ emoji.get(':poop:') } file.`,
+        message: `Delete ${ emoji.get(':poop:') }  file.`,
         command: 'rm /tmp/clonedbdump*'
       }
     ]
   },
   runSQL (config, sql) {
-    return `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.target.db.password ? `PGPASSWORD="${ config.target.db.password }"` : '' } psql -h localhost -U ${ config.target.db.username } -c "${ sql }" ${ config.target.db.database }`
+    return `${ config.target.ssh ? `ssh -p ${ config.target.port || 22 } ${ config.target.host } ` : '' }${ config.target.db.password ? `PGPASSWORD="${ config.target.db.password }"` : '' } psql -h ${ config.target.db.host ? config.target.db.host : 'localhost' } -U ${ config.target.db.username } -c "${ sql }" ${ config.target.db.database }`
   }
 }
